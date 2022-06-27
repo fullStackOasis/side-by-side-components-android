c422c17dd1fd58ae089047d096b9670c04ded961
fullStackOasis <fullstackdev@fullstackoasis.com>
2022-06-27 16:19:28 -0400

    Same, but with an EditText instead of TextView.

---
+++ app/src/main/java/com/fullstackoasis/sidebysidecomponentsdemo/RLEditTextActivity.java
@@ -0,0 +1,14 @@
+package com.fullstackoasis.sidebysidecomponentsdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RLEditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rl_edit_text);
+    }
+}
