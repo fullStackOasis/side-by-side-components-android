d7c2201c007b1fb73098e7c376afb8d75d106205
fullStackOasis <fullstackdev@fullstackoasis.com>
2022-06-27 16:06:39 -0400

    Initial commit. Demos how to use a RelativeLayout to place two widges right next to each other in Android (Java).

---
+++ README.md
@@ -0,0 +1,5 @@
+# Use RelativeLayout to create side-by-side TextView and Button in Android
+
+This is just a "sketch" with some demos of how you create a TextView next to a button.
+
+The TextView takes up most of the space, and the Button takes up the remaining space.
---
+++ app/src/main/java/com/fullstackoasis/sidebysidecomponentsdemo/RLActivity.java
@@ -0,0 +1,14 @@
+package com.fullstackoasis.sidebysidecomponentsdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rl);
+    }
+}
