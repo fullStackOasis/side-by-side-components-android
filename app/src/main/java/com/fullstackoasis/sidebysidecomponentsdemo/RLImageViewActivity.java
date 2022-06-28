3044cbb60ffc1bee8912243514935bb29784c02f
fullStackOasis <fullstackdev@fullstackoasis.com>
2022-06-27 20:18:26 -0400

    Default app now displays a checkmark with text alongside it

---
+++ app/src/main/java/com/fullstackoasis/sidebysidecomponentsdemo/RLImageViewActivity.java
@@ -0,0 +1,17 @@
+package com.fullstackoasis.sidebysidecomponentsdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RLImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rl_imageview);
+    }
+}
+
+
+
